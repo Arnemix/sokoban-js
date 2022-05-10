00071586fd305416484f86ee876cfa9d56bed129
58428 <58428@etu.he2b.be>
2022-05-10 11:28:15 +0200

    files game

---
+++ scripts/levels.js
@@ -0,0 +1,138 @@
+"use strict";
+
+/**
+ * @typedef {Object} Level Description d'un niveau de Sokoban.
+ * @property {"easy" | "medium" | "difficult"} difficulty La dificulté du jeu.
+ * @property {number} [best] Le nombre minimal de mouvement pour gagner.
+ * @property {string[]} map
+ * La carte du niveau, ligne par ligne, avec la signification suivante pour chaque symbole :
+ *   * `P` – le joueur,
+ *   * `x` – une cible,
+ *   * `#` – une boite,
+ *   * `@` – une boite sur une cible,
+ *   * ` ` – (espace) le sol,
+ *   * *autre* – un mur.
+ */
+
+/**
+ * Structure de données *globale* pour les données concernant les différents niveaux du jeu.
+ * @type {Level[]}
+ */
+const levels = [
+    {
+        difficulty: "easy",
+        best: 10,
+        map: [
+            "          ",
+            "   ┌─┐    ",
+            "   │x│    ",
+            "   │ └──┐ ",
+            " ┌─┘#P#x│ ",
+            " │x # ┌─┘ ",
+            " └──┐#│   ",
+            "    │x│   ",
+            "    └─┘   ",
+            "          ",
+        ],
+    },
+    {
+        difficulty: "easy",
+        best: 89,
+        map: [
+            "           ",
+            " ┌───┐     ",
+            " │P  │ ┌─┐ ",
+            " │ ##│ │x│ ",
+            " │ # └─┘x│ ",
+            " └┬┐    x│ ",
+            "  ├┘  ╷  │ ",
+            "  │   ├──┘ ",
+            "  │   │    ",
+            "  └───┘    ",
+            "           ",
+        ],
+    },
+    {
+        difficulty: "easy",
+        best: 33,
+        map: [
+            "        ",
+            "  ┌──┐  ",
+            " ┌┘  │  ",
+            " │P# │  ",
+            " ├┐# └┐ ",
+            " ├┘ # │ ",
+            " │x#  │ ",
+            " │xx@x│ ",
+            " └────┘ ",
+            "        ",
+        ],
+    },
+    {
+        difficulty: "medium",
+        best: 253,
+        map: [
+            "                     ",
+            "     ┌───┐           ",
+            "     │   │           ",
+            "     │#  │           ",
+            "   ┌─┘  #└┐          ",
+            "   │  # # │          ",
+            " ┌─┘ │ ┌┐ │   ┌────┐ ",
+            " │   │ └┘ └───┘  xx│ ",
+            " │ #  #          xx│ ",
+            " └───┐ ═══ ╷P┌┐  xx│ ",
+            "     │     ├───────┘ ",
+            "     └─────┘         ",
+            "                     ",
+        ],
+    },
+    {
+        difficulty: "medium",
+        map: [
+            "                 ",
+            " ┌────┬──────┐   ",
+            " │xx  │      └─┐ ",
+            " │xx  │ #  #   │ ",
+            " │xx  ╵#──┬┐   │ ",
+            " │xx    P └┘   │ ",
+            " │xx  ╷ ╷  #  ╶┤ ",
+            " └─┬──┘ └╴# #  │ ",
+            "   │ #  # # #  │ ",
+            "   │           │ ",
+            "   └───────────┘ ",
+            "                 ",
+        ],
+    },
+    {
+        difficulty: "medium",
+        map: [
+            "                    ",
+            "         ┌──────┐   ",
+            "         │     P│   ",
+            "         │ #═# ┌┘   ",
+            "         │ #  #│    ",
+            "         ├╴# # │    ",
+            " ┌──────┬┤ # ═ └─┐  ",
+            " │xxxx  └┘ #  #  │  ",
+            " ├╴xxx    #  #   │  ",
+            " │xxxx  ┌────────┘  ",
+            " └──────┘           ",
+            "                    ",
+        ],
+    },
+    {
+        difficulty: "difficult",
+        best: 57,
+        map: [
+            "              ",
+            "  ┌──┐  ┌───┐ ",
+            " ┌┘  │  │   │ ",
+            " │ # └──┘#  │ ",
+            " │  #xxxx # │ ",
+            " └┐    ╷ P ┌┘ ",
+            "  └────┴───┘  ",
+            "              ",
+        ],
+    },
+];
